fe570ad0a8ade69c88f1ca40641a0217279f2198
Gabegar <Gagarcia190@gmail.com>
2023-02-09 14:55:11 -0500

    removes: commented code @ constructor of tree

--- binary-search-trees/modules/Tree.js
+++ binary-search-trees/modules/Tree.js
@@ -1,19 +1,7 @@
 import Node from "./Node.js";
 
-/*
- * **** Balanced Binary Search Tree - Sorted Arrays *****
- * Set The middle element of the array as root.
- * Recursively do the same for the left half and right half.
- * Get the middle of the left half and make it the left child of the root created in step 1.
- * Get the middle of the right half and make it the right child of the root created in step 1.
- * Print the pre-order of the tree.
- */
-
 export default class Tree {
     constructor(array) {
-        // Initialize tree class with a sorted, non-repeating values, array.
-        // Sets the class attribute root, as the node returned from buildTree.
-        // If any empty array is passed, a new array with only the value of null is passed.
         const sortedArr = [...new Set(array)].sort((a, b) => (a > b ? 1 : -1));
         this.root = this._buildTree(sortedArr);
     }
